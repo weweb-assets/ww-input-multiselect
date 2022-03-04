fe5cdd31962f6b8c428f225846989976c381814b
Alexis JC <alexisjc.dev@gmail.com>
2022-03-04 10:51:11 +0100

    feat: multiselect first draft

--- src/wwElement.vue
+++ src/wwElement.vue
@@ -1,28 +1,134 @@
 <template>
-  <div class="my-element">
-    <p :style="textStyle">I am a custom element !</p>
+  <div class="input-multiselect" :class="{ editing: isEditing }">
+    <Multiselect
+      v-model="internalValue"
+      :options="selectOptions"
+      :close-on-select="content.closeOnSelect"
+      :searchable="content.searchable"
+      :mode="content.mode"
+      :disabled="content.disabled"
+      :hideSelected="content.hideSelected"
+    >
+      <template v-slot:tag="{ option, handleTagRemove }">
+        <div class="multiselect-tag" :style="option.style">
+          {{ option.label }}
+          <span
+            v-if="!content.disabled"
+            class="multiselect-tag-remove"
+            @mousedown.prevent="handleTagRemove(option, $event)"
+          >
+            <span class="multiselect-tag-remove-icon"></span>
+          </span>
+        </div>
+      </template>
+      <template v-if="content.mode === 'tags'" v-slot:option="{ option }">
+        <span class="multiselect-tag" :style="option.style">{{ option.label }}</span>
+      </template>
+    </Multiselect>
   </div>
 </template>
 
 <script>
+import Multiselect from '@vueform/multiselect'
+
+const DEFAULT_LABEL_FIELD = 'label'
+const DEFAULT_VALUE_FIELD = 'value'
+const DEFAULT_BG_COLOR_FIELD = 'bgColor'
+const DEFAULT_TEXT_COLOR_FIELD = 'textColor'
+
 export default {
+  components: { Multiselect },
+  emits: ['trigger-event', 'update:content:effect'],
   props: {
+    uid: { type: String, required: true },
     content: { type: Object, required: true },
+    /* wwEditor:start */
+    wwEditorState: { type: Object, required: true },
+    /* wwEditor:end */
+  },
+  setup(props) {
+    const { value: currentSelection, setValue: setCurrentSelection } = wwLib.wwVariable.useComponentVariable(
+        props.uid,
+        'currentSelection',
+        props.content.initialValue
+    );
+    return { currentSelection, setCurrentSelection };
   },
   computed: {
-    textStyle() {
-      return {
-        color: this.content.textColor,
-      };
+    isEditing() {
+      /* wwEditor:start */
+      return this.wwEditorState.editMode === wwLib.wwEditorHelper.EDIT_MODES.EDITION;
+      /* wwEditor:end */
+      // eslint-disable-next-line no-unreachable
+      return false;
     },
+    internalValue: {
+      get() {
+        return Array.isArray(this.currentSelection) ? this.currentSelection : []
+      },
+      set(value) {
+        this.setCurrentSelection(value)
+      }
+    },
+    selectOptions() {
+      const labelField = this.content.labelField || DEFAULT_LABEL_FIELD
+      const valueField = this.content.valueField || DEFAULT_VALUE_FIELD
+      const bgColorField = this.content.bgColorField || DEFAULT_BG_COLOR_FIELD
+      const textColorField = this.content.textColorField || DEFAULT_TEXT_COLOR_FIELD
+
+      if (!Array.isArray(this.content.options)) return []
+
+      return this.content.options.map(option => {
+        return typeof option === 'object' ? {
+          label: wwLib.wwManagerLang.getText(option[labelField]), 
+          value: option[valueField],
+          style: {
+            backgroundColor: option[bgColorField] || this.content.tagsDefaultBgColor,
+            color: option[textColorField] || this.content.tagsDefaultTextColor
+          }
+        } : { // to allow flat array
+          label: option, 
+          value: option,
+          style: {
+            backgroundColor: this.content.tagsDefaultBgColor,
+            color: this.content.tagsDefaultTextColor
+          }
+        }
+      })
+    },
+  },
+  watch: {
+    'content.initialValue'(value) {
+      this.setCurrentSelection(value)
+    },
+    currentSelection(value, oldValue) {
+      this.$emit('trigger-event', { name: 'change', event: { value } });
+    },
+    /* wwEditor:start */
+    'wwEditorState.boundProps.options'(isBind) {
+        if (!isBind) this.$emit('update:content:effect', { displayField: null, valueField: null, bgColorField: null, textColorField: null });
+    },
+    selectOptions() {
+      // multi select breaks when selection and available options mismatch
+      this.setCurrentSelection([])
+    }
+    /* wwEditor:end */
   },
 };
 </script>
 
-<style lang="scss" scoped>
-.my-element {
-  p {
-    font-size: 18px;
+<style src="@vueform/multiselect/themes/default.css"></style>
+
+<style type="scss">
+.input-multiselect {
+  /* wwEditor:start */
+  &.editing {
+    pointer-events: none;
   }
+  /* wwEditor:end */
+}
+.multiselect-tag {
+  padding: 4px;
+  border-radius: 4px;
 }
 </style>
--- ww-config.js
+++ ww-config.js
@@ -1,16 +1,224 @@
 export default {
-  editor: {
-    label: {
-      en: "My Element",
+    editor: {
+        label: {
+            en: "Input multiselect",
+            fr: "Champs sélection multiple",
+        },
     },
-  },
-  properties: {
-    textColor: {
-      label: {
-        en: "Text color",
-      },
-      type: "Color",
-      defaultValue: "#F23636",
-    },
-  },
+    triggerEvents: [
+        { name: 'change', label: { en: 'On change' }, event: { value: [] } },
+    ],
+    properties: {
+        tagsDefaultBgColor: {
+            label: {
+                en: 'Default bg color (tags)',
+                fr: 'Couleur de fond des tags'
+            },
+            type: 'Color',
+            defaultValue: '#099AF2'
+        },
+        tagsDefaultTextColor: {
+            label: {
+                en: 'Default text color (tags)',
+                fr: 'Couleur du texte des tags'
+            },
+            type: 'Color',
+            defaultValue: '#FFFFFF'
+        },
+        disabled: {
+            label: {
+                en: 'Disabled',
+                fr: 'Désactivé'
+            },
+            type: 'OnOff',
+            defaultValue: false,
+            section: 'settings',
+            bindable: true
+        },
+        mode: {
+            label: {
+                en: 'Mode',
+                fr: 'Mode'
+            },
+            type: 'TextSelect',
+            defaultValue: 'tags',
+            options: {
+                options: [{label: 'Tags', value: 'tags'}, {label: 'Counter', value: 'multiple'}],
+            },
+            section: 'settings',
+            bindable: true
+        },
+        hideSelected: {
+            label: {
+                en: 'Hide selected elements',
+                fr: 'Cacher les éléments sélectionnés'
+            },
+            type: 'OnOff',
+            defaultValue: true,
+            section: 'settings',
+        },
+        searchable: {
+            label: {
+                en: 'Searchable',
+                fr: 'Recherche'
+            },
+            type: 'OnOff',
+            defaultValue: true,
+            section: 'settings',
+        },
+        closeOnSelect: {
+            label: {
+                en: 'Close on select',
+                fr: 'Ferme à la sélection'
+            },
+            type: 'OnOff',
+            defaultValue: false,
+            section: 'settings',
+        },
+        initialValue: {
+            label: {
+                en: 'Initial value',
+                fr: 'Valeur initiale'
+            },
+            type: 'Array',
+            bindable: true,
+            section: 'settings',
+        },
+        options: {
+            label: { en: 'Options', fr: 'Options' },
+            type: 'Array',
+            section: 'settings',
+            options: {
+                item: {
+                    type: 'Object',
+                    defaultValue: { label: '', value: '', bgColor: null, textColor: null },
+                    options: {
+                        item: {
+                            label: {
+                                label: { en: 'Label' },
+                                type: 'Text',
+                                options: { placeholder: 'Label' },
+                                multiLang: true,
+                            },
+                            value: {
+                                label: { en: 'Value' },
+                                type: 'Text',
+                                options: { placeholder: 'Value' },
+                            },
+                            bgColor: {
+                                label: { en: 'Background color' },
+                                type: 'Color',
+                            },
+                            textColor: {
+                                label: { en: 'Text color' },
+                                type: 'Color',
+                            },
+                        }
+                    },
+                },
+            },
+            defaultValue: [
+                { value: 'option', label: { en: 'option - 1' }, bgColor: null, textColor: null },
+                { value: 'option2', label: { en: 'option - 2' }, bgColor: null, textColor: null },
+            ],
+            bindable: true,
+        },
+        hintFields: {
+            hidden: (content, sidepanelContent, boundProps) => !boundProps.options || content.options,
+            label: {
+                en: 'Options fields',
+                fr: 'Champs des options',
+            },
+            type: 'Info',
+            options: {
+                text: { en: 'Please provide at least one option to configure fields' },
+            },
+            editorOnly: true,
+            section: 'settings',
+        },
+        labelField: {
+            hidden: (content, sidepanelContent, boundProps) => !boundProps.options || !content.options,
+            label: {
+                en: 'Label field',
+                fr: 'Label field',
+            },
+            type: 'TextSelect',
+            options: content => {
+                const properties = [{ value: null, label: { en: 'Select a property' } }];
+
+                if (content.options && typeof content.options[0] === 'object') {
+                    properties.push(
+                        ...Object.keys(content.options[0]).map(key => ({ value: key, label: { en: key } }))
+                    );
+                }
+
+                return { options: properties };
+            },
+            defaultValue: null,
+            section: 'settings',
+        },
+        valueField: {
+            hidden: (content, sidepanelContent, boundProps) => !boundProps.options || !content.options,
+            label: {
+                en: 'Value field',
+                fr: 'Value field',
+            },
+            type: 'TextSelect',
+            options: content => {
+                const properties = [{ value: null, label: { en: 'Select a property' } }];
+
+                if (content.options && typeof content.options[0] === 'object') {
+                    properties.push(
+                        ...Object.keys(content.options[0]).map(key => ({ value: key, label: { en: key } }))
+                    );
+                }
+
+                return { options: properties };
+            },
+            defaultValue: null,
+            section: 'settings',
+        },
+        bgColorField: {
+            hidden: (content, sidepanelContent, boundProps) => !boundProps.options || !content.options,
+            label: {
+                en: 'Bg color field',
+                fr: 'Bg color field',
+            },
+            type: 'TextSelect',
+            options: content => {
+                const properties = [{ value: null, label: { en: 'Select a property' } }];
+
+                if (content.options && typeof content.options[0] === 'object') {
+                    properties.push(
+                        ...Object.keys(content.options[0]).map(key => ({ value: key, label: { en: key } }))
+                    );
+                }
+
+                return { options: properties };
+            },
+            defaultValue: null,
+            section: 'settings',
+        },
+        textColorField: {
+            hidden: (content, sidepanelContent, boundProps) => !boundProps.options || !content.options,
+            label: {
+                en: 'Text color field',
+                fr: 'Text color field',
+            },
+            type: 'TextSelect',
+            options: content => {
+                const properties = [{ value: null, label: { en: 'Select a property' } }];
+
+                if (content.options && typeof content.options[0] === 'object') {
+                    properties.push(
+                        ...Object.keys(content.options[0]).map(key => ({ value: key, label: { en: key } }))
+                    );
+                }
+
+                return { options: properties };
+            },
+            defaultValue: null,
+            section: 'settings',
+        },
+    }
 };
