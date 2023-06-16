6d23f42a487b56f5ca90ac274588b60e9f4ac3a7
Marc Fabre <nds.kram@gmail.com>
2023-06-16 12:17:28 +0200

    Corrected default color

--- src/wwElement.vue
+++ src/wwElement.vue
@@ -307,8 +307,6 @@ export default {
         formatOption(option) {
             const labelField = this.content.labelField || DEFAULT_LABEL_FIELD;
             const valueField = this.content.valueField || DEFAULT_VALUE_FIELD;
-            const bgColorField = this.content.bgColorField || DEFAULT_BG_COLOR_FIELD;
-            const textColorField = this.content.textColorField || DEFAULT_TEXT_COLOR_FIELD;
 
             if (this.layoutType === 'free')
                 return {
@@ -321,13 +319,6 @@ export default {
                 ? {
                       label: wwLib.wwLang.getText(wwLib.resolveObjectPropertyPath(option, labelField)),
                       value: wwLib.resolveObjectPropertyPath(option, valueField),
-                      style: {
-                          backgroundColor:
-                              wwLib.resolveObjectPropertyPath(option, bgColorField) || this.content.tagsDefaultBgColor,
-                          color:
-                              wwLib.resolveObjectPropertyPath(option, textColorField) ||
-                              this.content.tagsDefaultTextColor,
-                      },
                       data: option,
                   }
                 : {
@@ -337,7 +328,7 @@ export default {
                   };
         },
         getOptionStyle(option) {
-            return this.layoutType === 'text' ? option.style || this.defaultTagStyle : null;
+            return this.layoutType === 'text' ? this.defaultTagStyle : null;
         },
         handleOpening(value) {
             if (!this.$refs.multiselect) return;
