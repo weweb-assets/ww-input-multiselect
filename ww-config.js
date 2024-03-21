efdb5abb3c372ec65dcde6740177a0c8506a023c
Kévin Barfleur <39409397+kevinbarfleur@users.noreply.github.com>
2024-03-21 10:40:38 +0100

    fix

--- ww-config.js
+++ ww-config.js
@@ -372,7 +372,6 @@ export default {
             },
             type: 'OnOff',
             defaultValue: false,
-            bindable: true,
             section: 'settings',
             /* wwEditor:start */
             bindingValidation: {
